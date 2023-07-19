9f8422d484cfbf25a444ba20755db05937c50f29
PrabhjyotSinghGill <prabhjyotg@gmail.com>
2023-07-19 11:38:50 -0400

    stocks : search bar in progress

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import './App.css';
+import "./App.css";
+import { Button, ButtonGroup } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import CloseIcon from "@mui/icons-material/Close";
+import { TextField } from "@mui/material";
 
 function App() {
   return (
     <div className="app">
-      
+      <div className="header">
+        <h1>Stocks</h1>
+        <div className="searchBar">
+          <TextField
+            className="searchQuery"
+            id="standard-basic"
+            label="search"
+            variant="standard"
+          />
+          <ButtonGroup variant="text" className="searchButton">
+            <Button>
+              <CloseIcon></CloseIcon>
+            </Button>
+            <Button>
+              <SearchIcon></SearchIcon>
+            </Button>
+          </ButtonGroup>
+        </div>
+      </div>
     </div>
   );
 }
