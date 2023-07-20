76f71510901c5e4709c885caf5557922db099dcb
PrabhjyotSinghGill <prabhjyotg@gmail.com>
2023-07-20 18:43:09 -0400

    adds more tests for recommendation algo

--- src/utils/recemmendation.test.js
+++ src/utils/recemmendation.test.js
@@ -13,6 +13,31 @@ const MOCK_DATA = {
         price: [100, 120, 110, 115, 105, 200, 220, 210, 215, 205],
         socialMediaCount: [50, 55, 60, 65, 70, 0, 0, 0, 0, 0],
     },
+    BUY_TEST_1: {
+        //Price remains same but social media count increases in second half
+        price: [100, 120, 110, 115, 105, 100, 120, 110, 115, 105],
+        socialMediaCount: [0, 0, 0, 0, 0, 50, 55, 60, 65, 70],
+    },
+    BUY_TEST_2: {
+        //Price increases & social media count increases in second half
+        price: [100, 120, 110, 115, 105, 200, 220, 210, 215, 205],
+        socialMediaCount: [0, 0, 0, 0, 0, 50, 55, 60, 65, 70],
+    },
+    SELL_TEST_1: {
+        //Price remains same but social media count decreases in second half
+        price: [100, 120, 110, 115, 105, 100, 120, 110, 115, 105],
+        socialMediaCount: [50, 55, 60, 65, 7, 0, 0, 0, 0, 0],
+    },
+    SELL_TEST_2: {
+        //Price decreases but social media count increases in second half
+        price: [100, 120, 110, 115, 105, 50, 60, 55, 50, 40],
+        socialMediaCount: [0, 0, 0, 0, 0, 50, 55, 60, 65, 7],
+    },
+    SELL_TEST_3: {
+        //Price decreases & social media count also decreases in second half
+        price: [100, 120, 110, 115, 105, 50, 60, 55, 50, 40],
+        socialMediaCount: [50, 55, 60, 65, 7, 0, 0, 0, 0, 0],
+    },
 }
 
 jest.mock('../data/api', () => {
@@ -42,5 +67,30 @@ describe('getStockRecommendation', () => {
     it('should return HOLD if prices are increasing and social media counts are decreasing', async () => {
         const recommendation = await getStockRecommendation('HOLD_TEST_3')
         expect(recommendation).toBe(RECOMMENDATION.HOLD)
+    })
+
+    it('should return BUY if prices remains same but social media counts are increasing', async () => {
+        const recommendation = await getStockRecommendation('BUY_TEST_1')
+        expect(recommendation).toBe(RECOMMENDATION.BUY)
+    })
+
+    it('should return BUY if prices increases & social media counts are also increasing', async () => {
+        const recommendation = await getStockRecommendation('BUY_TEST_2')
+        expect(recommendation).toBe(RECOMMENDATION.BUY)
+    })
+
+    it('should return SELL if prices remains same but social media counts are decreasing', async () => {
+        const recommendation = await getStockRecommendation('SELL_TEST_1')
+        expect(recommendation).toBe(RECOMMENDATION.SELL)
+    })
+
+    it('should return SELL if prices decreases but social media counts are increasing', async () => {
+        const recommendation = await getStockRecommendation('SELL_TEST_2')
+        expect(recommendation).toBe(RECOMMENDATION.SELL)
+    })
+
+    it('should return SELL if prices decreases & social media counts are also decreasing', async () => {
+        const recommendation = await getStockRecommendation('SELL_TEST_3')
+        expect(recommendation).toBe(RECOMMENDATION.SELL)
     })
 })
