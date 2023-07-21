ac4fc90a9d4902aa439a8c48e502f7b0d629ae85
PrabhjyotSinghGill <prabhjyotg@gmail.com>
2023-07-20 23:13:03 -0700

    add description for logic behind recommendation algorithm

--- src/utils/recommendation.js
+++ src/utils/recommendation.js
@@ -16,23 +16,31 @@ export const getStockRecommendation = (
         Math.abs(socialMediaCountChangePercentage) <
             percentageSocialMediaCountChangeThreshold
     ) {
+        // If the price change is less than 5% or social media count change is less than 10%,
         return RECOMMENDATION.HOLD
     } else if (
         priceChangePercentage > 0 &&
         socialMediaCountChangePercentage > 0
     ) {
+        // If the price change is greater than 5% and social media count change is greater than 10%,
+        // Positive sentiment
         return RECOMMENDATION.BUY
     } else if (
         priceChangePercentage < 0 &&
         socialMediaCountChangePercentage < 0
     ) {
+        // If the price decreases by more than 5% and social media count also decreases by more than 10%,
+        // Negative sentiment
         return RECOMMENDATION.SELL
     } else if (
         priceChangePercentage < 0 &&
         socialMediaCountChangePercentage > 0
     ) {
+        // If the price decreases by more than 5% and social media count increases by more than 10%,
+        // Something wrong with the stock as price decreased rapidly and lot of people are talking about it.
         return RECOMMENDATION.SELL
     } else {
+        // If the price change and social media count change are in opposite direction,
         return RECOMMENDATION.HOLD
     }
 }
