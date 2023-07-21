a80180c2af21fbff707905c2b70805b0f90a0d11
PrabhjyotSinghGill <prabhjyotg@gmail.com>
2023-07-20 23:20:51 -0700

    adds tests for recommendation algorithm

--- src/utils/recemmendation.test.js
+++ src/utils/recemmendation.test.js
@@ -1,13 +1,33 @@
 import { getStockRecommendation, RECOMMENDATION } from './recommendation'
 
 describe('getStockRecommendation', () => {
-    it('should return BUY if price goes up and socialMediaCount goes up', async () => {
-        const recommendation = await getStockRecommendation(10, 10)
+    it('returns HOLD when price and social media count changes are within threshold', () => {
+        const recommendation = getStockRecommendation(3, 8)
+        expect(recommendation).toBe(RECOMMENDATION.HOLD)
+    })
+
+    it('returns HOLD when both price change and social media count change are negative but within threshold', () => {
+        const recommendation = getStockRecommendation(-4, -8)
+        expect(recommendation).toBe(RECOMMENDATION.HOLD)
+    })
+
+    it('returns BUY when price change exceeds threshold and positive social media count change', () => {
+        const recommendation = getStockRecommendation(6, 15)
         expect(recommendation).toBe(RECOMMENDATION.BUY)
     })
 
-    it('should return SELL if prices goes down and social media count goes down', async () => {
-        const recommendation = await getStockRecommendation(-10, -10)
+    it('returns SELL both price and social media count decrease by more than threshold', () => {
+        const recommendation = getStockRecommendation(-6, -11)
         expect(recommendation).toBe(RECOMMENDATION.SELL)
+    })
+
+    it('returns SELL when price decrease exceeds threshold and opposite direction social media count change', () => {
+        const recommendation = getStockRecommendation(-10, 20)
+        expect(recommendation).toBe(RECOMMENDATION.SELL)
+    })
+
+    it('returns HOLD when price change exceeds threshold and negative social media count change', () => {
+        const recommendation = getStockRecommendation(7, -12)
+        expect(recommendation).toBe(RECOMMENDATION.HOLD)
     })
 })
